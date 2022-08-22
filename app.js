5695a051b297e37a6228a7989409fd9a51d1b26d
vaishnavrodi <vaishnavrodi@gmail.com>
2022-08-23 01:45:20 +0530

    Added the password encryption and enviornment variables

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -13,18 +15,16 @@ app.use(express.urlencoded({extended:true}));
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser:true});
 
-
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email : String,
   password: String
-};
+});
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
+
 
 const User = new mongoose.model('User', userSchema);
-
-
-
-
-
 
 
 
@@ -77,14 +77,6 @@ app.post('/login', (req,res) => {
 
   });
 });
-
-
-
-
-
-
-
-
 
 
 
